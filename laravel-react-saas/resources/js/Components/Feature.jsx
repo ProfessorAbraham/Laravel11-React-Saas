215b00f26d572ebc37a22e8f42f0323bf5a55812
ProfessorAbraham <abrahamdagne09@gmail.com>
2024-10-24 12:13:37 -0700

    Update Feature.jsx

--- laravel-react-saas/resources/js/Components/Feature.jsx
+++ laravel-react-saas/resources/js/Components/Feature.jsx
@@ -15,6 +15,43 @@ export default function Feature() {
             }
         >
             <Head title="Feature 1" />
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    {answer !== null && (
+                        <div className="mb-3 py-3 px-5 rounded text-white bg-emerald-600 text-xl">
+                            Result of Calculations: {answer}
+                        </div>
+                    )}
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg relative">
+                        {availablecredits !== null &&
+                            Feature.required_credits > availabledCredits && (
+                                <div className="absolute left-0 top-0 right-0 bottom-0 z-20 flex flex-col items-center justify-center bg-white/70 gap-3">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
+                                        stroke="currentColor"
+                                        className="size-6"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                                        />
+                                    </svg>
+                                    <div>
+                                        You don't have enough credits to access
+                                        this feature. Go{" "}
+                                        <Link href="/" className="underline">
+                                            Buy More Credits
+                                        </Link>
+                                    </div>
+                                </div>
+                            )}
+                    </div>
+                </div>
+            </div>
         </AuthenticatedLayout>
     );
 }
